3abaa79fe32d7ed4da13ff580b1aa6057216d5a3
Alan Reid <alanreidt@gmail.com>
2019-05-31 17:22:13 +0300

    Added 3rd-party libraries (nouislider, wnumb)

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,7 @@
 import $ from 'jquery';
+import wNumb from 'wnumb';
+import noUiSlider from 'nouislider';
+import '../../node_modules/nouislider/distribute/nouislider.css';
 
 $(document).ready(function() {
   // There would be JQuery code;
