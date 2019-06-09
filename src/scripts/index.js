e107f7afa99f6c74d1ec0fe662d35855728dda8c
Alan Reid <alanreidt@gmail.com>
2019-06-09 14:29:46 +0300

    Added 'datepicker' block

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,9 @@ import '../../node_modules/jquery-ui-dist/jquery-ui.css';
 var jquery = require('jquery');
 window.$ = window.jQuery = jquery;
 require('jquery-ui-dist/jquery-ui');
+require('../blocks/common.blocks/datepicker/i18n/datepicker-zh-CN.js');
+require('../blocks/common.blocks/datepicker/i18n/datepicker-fr.js');
+require('../blocks/common.blocks/datepicker/i18n/datepicker-ru.js');
 import wNumb from 'wnumb';
 import noUiSlider from 'nouislider';
 import '../../node_modules/nouislider/distribute/nouislider.css';
@@ -15,29 +18,29 @@ var rangeSliderValues;
 var rangeSliderRange = '';
 
 noUiSlider.create(rangeSlider, {
-    start: [5000, 10000],
-    step: 500,
-    margin: 500,
-    // orientation: 'vertical',
-    connect: true,
-    range: {
-      'min': 0,
-      'max': 15000
-    },
-    ariaFormat: wNumb({
-      decimals: 0
-    }),
-    format: wNumb({
-      decimals: 0,
-      thousand: ' ',
-      suffix: '₽'
-    })
+  start: [5000, 10000],
+  step: 500,
+  margin: 500,
+  // orientation: 'vertical',
+  connect: true,
+  range: {
+    'min': 0,
+    'max': 15000
+  },
+  ariaFormat: wNumb({
+    decimals: 0
+  }),
+  format: wNumb({
+    decimals: 0,
+    thousand: ' ',
+    suffix: '₽'
+  })
 });
 
 rangeSlider.noUiSlider.on('update', function () {
   rangeSliderValues = rangeSlider.noUiSlider.get();
 
-  rangeSliderRange = rangeSliderValues[0] +'–'+ rangeSliderValues[1];
+  rangeSliderRange = rangeSliderValues[0] + '–' + rangeSliderValues[1];
 
   rangeSliderOutput.innerHTML = rangeSliderRange;
 });
@@ -57,6 +60,62 @@ rangeSlider.noUiSlider.on('update', function () {
 
 /* JQuery */
 
-$(document).ready(function() {
-  $( "#date" ).datepicker();
+$(document).ready(function () {
+
+  $(function () {
+    $.datepicker.setDefaults( $.datepicker.regional[ 'ru' ] );
+
+    $.datepicker.setDefaults({
+      showOtherMonths: true,
+      selectOtherMonths: true,
+      showButtonPanel: true,
+      closeText: 'Применить',
+      currentText: 'Очистить'
+    });
+
+    var dateFormat = "dd.mm.yy",
+      from = $("#from-date")
+        .datepicker({
+          // showButtonPanel: true
+          /* firstDay: 1,
+          showOtherMonths: true,
+          selectOtherMonths: true,
+          showButtonPanel: true,
+          closeText: 'Применить',
+          currentText: 'Очистить',
+          dateFormat: dateFormat */
+
+          minDate: 0
+        })
+        .on("change", function () {
+          to.datepicker("option", "minDate", getDate(this));
+        }),
+      to = $("#to-date")
+        .datepicker({
+          // showButtonPanel: true
+          /* firstDay: 1,
+          showOtherMonths: true,
+          selectOtherMonths: true,
+          showButtonPanel: true,
+          closeText: 'Применить',
+          currentText: 'Очистить',
+          dateFormat: dateFormat */
+
+          minDate: 1
+        })
+        .on("change", function () {
+          from.datepicker("option", "maxDate", getDate(this));
+        });
+
+    function getDate(element) {
+      var date;
+      try {
+        date = $.datepicker.parseDate(dateFormat, element.value);
+      } catch (error) {
+        date = null;
+      }
+
+      return date;
+    }
+  });
 });
