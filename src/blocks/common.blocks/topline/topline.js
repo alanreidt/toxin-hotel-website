c7cce787ecaf072110204832ca7e282ad9745f76
Alan Reid <alanreidt@gmail.com>
2019-07-15 19:23:25 +0300

    Edited 'topline' block's responsive styles

--- src/blocks/common.blocks/topline/topline.js
+++ src/blocks/common.blocks/topline/topline.js
@@ -1,16 +1,20 @@
 $(function () {
   var menu = $('#menu');
   var menuButton = $('#menu-button');
-
-  $('.page__body').on('click', function () {
-    // if (menuButton.hasClass('topline__menu_is-active')) {
-    //   menu.toggleClass('topline__reception_is-active');
-    //   menuButton.toggleClass('topline__menu_is-active');
-    // }
-  });
+  var menuOverlay = $('#menu-overlay');
+  var page = $('#page');
 
   $(menuButton).on('click', function () {
     menu.toggleClass('topline__reception_is-active');
-    menuButton.toggleClass('topline__menu_is-active');
+    menuButton.toggleClass('menu-button_is-clicked');
+    menuOverlay.toggleClass('menu-overlay_is-active');
+    page.toggleClass('page_is-unscrollable');
+  });
+
+  $(menuOverlay).on('click', function () {
+    menu.toggleClass('topline__reception_is-active');
+    menuButton.toggleClass('menu-button_is-clicked');
+    menuOverlay.toggleClass('menu-overlay_is-active');
+    page.toggleClass('page_is-unscrollable');
   });
 });
