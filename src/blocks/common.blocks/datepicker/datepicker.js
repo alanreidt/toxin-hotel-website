f1eff0cb261d5bc87e06d5d09c2532505a35767c
Alan Reid <alanreidt@gmail.com>
2019-12-11 00:48:58 +0300

    Modify datepicker common block Remove redundant scripts

--- src/blocks/common.blocks/datepicker/datepicker.js
+++ src/blocks/common.blocks/datepicker/datepicker.js
@@ -1,35 +1,3 @@
-$(function () {
-  var datePickerOutput = $("#js-datepicker__date-output").datepicker({
-    showOtherMonths: true,
-    selectOtherMonths: true,
-    prevText: '',
-    nextText: '',
-    showButtonPanel: true,
-    closeText: 'Применить',
-    currentText: 'Очистить',
-    minDate: 0
-  }).hide();
-
-  $(".js-datepicker__date-input").on("focusin", function () {
-    datePickerOutput.show();
-  });
-
-  $(".js-datepicker__date-input").on("focusout", function () {
-    datePickerOutput.hide();
-  });
-
-  datePickerOutput.on("change", function () {
-    // there would be code
-  });
-
-  function getDate(element) {
-    var date;
-    try {
-      date = $.datepicker.parseDate(dateFormat, element.value);
-    } catch (error) {
-      date = null;
-    }
-
-    return date;
-  }
-});
+require('./i18n/datepicker-zh-CN.js');
+require('./i18n/datepicker-fr');
+require('./i18n/datepicker-ru');
