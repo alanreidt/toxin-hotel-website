71abb1ef0ea8cd4e080340ff6e84cf49d6c8aa34
Alan Reid <alanreidt@gmail.com>
2019-12-11 18:57:25 +0300

    Modify room-details.js Add imports of blocks

---
+++ src/scripts/room-details.js
@@ -0,0 +1,5 @@
+import './base.js';
+import '../blocks/common.blocks/input-field/input-field.js';
+import '../blocks/common.blocks/dropdown-field/dropdown-field.js';
+import '../blocks/common.blocks/search-box/__dropdown/search-box__dropdown.js';
+import '../blocks/common.blocks/search-box/__datepicker/search-box__datepicker.js';
