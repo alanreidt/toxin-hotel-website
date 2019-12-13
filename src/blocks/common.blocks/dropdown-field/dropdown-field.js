38cf9ee6188e630239cf3647f400d8bc3d4123d3
Alan Reid <alanreidt@gmail.com>
2019-12-13 23:56:51 +0300

    Modify dropdown-field common block Replace js code with a class

--- src/blocks/common.blocks/dropdown-field/dropdown-field.js
+++ src/blocks/common.blocks/dropdown-field/dropdown-field.js
@@ -1,28 +1,28 @@
-var jquery = require('jquery');
-window.$ = window.jQuery = jquery;
+export class DropdownField {
 
+  constructor($parent) {
+    this.$parent = $parent;
+    this.$optionList = $parent.querySelector(".dropdown-field__dropdown");
+    this.$removeButton = $parent.querySelector(".dropdown-field__button_remove");
+    this.$addButton = $parent.querySelector(".dropdown-field__button_add");
+    this.$quantity = $parent.querySelector(".dropdown-field__quantity");
 
-$(function () {
-  var dropDown = $('#js-dropdown-field__content');
-
-  $('#dropdown-field-input').on('focusin', function () {
-    dropDown.toggleClass('dropdown-field_is-expanded');
-  });
-
-  $('#dropdown-field-input').on('focusout', function () {
-    dropDown.toggleClass('dropdown-field_is-expanded');
-  });
-});
-
-$(function () {
-  var quantity = $('.dropdown-field__quantity').html();
-  var maxValue = '5';
-
-  if (quantity === '0') {
-    $('.dropdown-field__button_remove').addClass('dropdown-field__button_is-disabled');
+    $parent.addEventListener( "focusin", this.handleParentFocusIn.bind(this) );
   }
 
-  if (quantity === maxValue) {
-    $('.dropdown-field__button_add').addClass('dropdown-field__button_is-disabled');
+
+  handleParentFocusIn() {
+    this.$parent.classList.add("dropdown-field_is-expanded");
+
+    this.$optionList.addEventListener( "click", this.handleOptionListClick.bind(this) );
   }
-});
+
+
+  handleOptionListClick(event) {
+    let quantity = parseFloat( this.$quantity.textContent );
+
+    this.$quantity.textContent = ( event.target === this.$addButton ) ? ++quantity :
+      ( event.target === this.$removeButton ) ? --quantity : quantity;
+  }
+
+}
