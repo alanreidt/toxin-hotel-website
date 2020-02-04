1868a96e7800e88c85336bbbde380d0072b74b7c
Alan Reid <alanreidt@gmail.com>
2020-02-04 12:16:25 +0300

    Update dropdown-field common block Add _handDocumentClick method

--- src/blocks/common.blocks/dropdown-field/dropdown-field.js
+++ src/blocks/common.blocks/dropdown-field/dropdown-field.js
@@ -7,9 +7,20 @@ export class DropdownField {
 
   _addHostEventListener() {
     this.$host.addEventListener("focusin", this._handleParentFocusIn.bind(this));
+    document.addEventListener("click", this._handleDocumentClick.bind(this));
   }
 
   _handleParentFocusIn() {
     this.$host.classList.add("dropdown-field_is-expanded");
+  }
+
+  _handleDocumentClick(event) {
+    const target = event.target.closest(".js-dropdown-field");
+
+    console.log(target);
+
+    if (target !== this.$host) {
+      this.$host.classList.remove("dropdown-field_is-expanded");
+    }
   }
 }
