6cbb8c75e439d06224afb5f404629b5a503b1ea5
Alan Reid <alanreidt@gmail.com>
2020-03-03 17:44:59 +0300

    Update date-mask.init.js Replace strict dates with ones derived from date object

--- src/blocks/common.blocks/date-mask/date-mask.init.js
+++ src/blocks/common.blocks/date-mask/date-mask.init.js
@@ -1,14 +1,19 @@
 import Cleave from 'cleave.js';
 
 window.addEventListener("load", () => {
+  const now = new Date();
+  const nowISOString = now.toISOString();
+  const afterTwoYearsFromNow = new Date(now.getFullYear() + 2, now.getMonth(), now.getDate());
+  const afterTwoYearsFromNowISOString = afterTwoYearsFromNow.toISOString();
+
   const dateMasks = document.querySelectorAll(".js-date-mask");
 
   dateMasks.forEach((dateMask) => {
     new Cleave(dateMask, {
       date: true,
       delimiter: '.',
-      dateMin: '2019-01-01',
-      dateMax: '2022-12-31',
+      dateMin: nowISOString,
+      dateMax: afterTwoYearsFromNowISOString,
       datePattern: ['d', 'm', 'Y'],
     });
   });
