b7094291f10919f1c88d83a41b2b516647b8bd1e
Alan Reid <alanreidt@gmail.com>
2020-03-03 16:51:32 +0300

    Update date-mask.init.js Wrapped file body into function of window load event listener

--- src/blocks/common.blocks/date-mask/date-mask.init.js
+++ src/blocks/common.blocks/date-mask/date-mask.init.js
@@ -1,13 +1,15 @@
 import Cleave from 'cleave.js';
 
-const dateMasks = document.querySelectorAll(".js-date-mask");
+window.addEventListener("load", () => {
+  const dateMasks = document.querySelectorAll(".js-date-mask");
 
-dateMasks.forEach((dateMask) => {
-  new Cleave(dateMask, {
-    date: true,
-    delimiter: '.',
-    dateMin: '2019-01-01',
-    dateMax: '2022-12-31',
-    datePattern: ['d', 'm', 'Y'],
+  dateMasks.forEach((dateMask) => {
+    new Cleave(dateMask, {
+      date: true,
+      delimiter: '.',
+      dateMin: '2019-01-01',
+      dateMax: '2022-12-31',
+      datePattern: ['d', 'm', 'Y'],
+    });
   });
 });
