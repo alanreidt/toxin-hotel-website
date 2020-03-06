4b188831c4bc003d4583b06b1368e29f2caaceb7
Alan Reid <alanreidt@gmail.com>
2020-03-06 16:47:38 +0300

    Add logic to Counter component Model

--- src/blocks/common.blocks/counter/Model.js
+++ src/blocks/common.blocks/counter/Model.js
@@ -1,11 +1,25 @@
-import deepEqual from 'deep-equal';
+import flow from 'lodash/fp/flow';
+import map from 'lodash/fp/map';
+import sortBy from 'lodash/fp/sortBy';
+import filter from 'lodash/fp/filter';
+import identity from 'lodash/fp/identity';
 import cloneDeep from 'lodash/cloneDeep';
+import deepEqual from 'deep-equal';
+
+import {
+  eitherOr,
+  placeBetween,
+  crossWith,
+} from '../../../../modules/utilities/helpers';
 
 import { ObserverMixin } from '../../../../modules/utilities';
 
 class Model {
-  constructor(options) {
-    this._options = {};
+  constructor(options = {}) {
+    this._options = {
+      boundaries: [0, 5],
+      value: 0,
+    };
 
     this.setOptions(options);
   }
@@ -17,9 +31,49 @@ class Model {
   setOptions(options) {
     if (deepEqual(this._options, options)) return;
 
-    this._options = options;
+    Object.keys(this._options).forEach((key) => {
+      const keyFirstLetter = key[0];
+      const isKeyPrivate = keyFirstLetter === '_';
+
+      if (isKeyPrivate) return;
+
+      const capitalizedKey = keyFirstLetter.toUpperCase() + key.slice(1);
+      const keySetterName = `_set${capitalizedKey}`;
+      const value = options[key];
+
+      this[keySetterName](value);
+    });
 
     this.triggerSubscribers('update', this.getOptions());
+
+    return this;
+  }
+
+  _setBoundaries(values) {
+    const currentValue = this._options.boundaries;
+    const newValues = [].concat(values);
+
+    const validate = flow(
+      sortBy(identity),
+      map(parseFloat),
+      filter(Number.isFinite),
+      crossWith(currentValue),
+    );
+
+    this._options.boundaries = validate(newValues);
+  }
+
+  _setValue(value) {
+    const [min, max] = this._options.boundaries;
+    const currentValue = this._options._value;
+
+    const validate = flow(
+      parseFloat,
+      eitherOr(currentValue),
+      placeBetween(min, max),
+    );
+
+    this._options.value = validate(value);
   }
 }
 
