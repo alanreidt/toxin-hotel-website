0c97421865c4d194e045c269683e21c3a80d324b
Alan Reid <alanreidt@gmail.com>
2020-03-06 13:20:35 +0300

    Update Counter.js Add _bindMethods method

--- src/blocks/common.blocks/counter/Counter.js
+++ src/blocks/common.blocks/counter/Counter.js
@@ -3,6 +3,7 @@ export class Counter {
     this.anchorElement = anchorElement;
 
     this._assignElements();
+    this._bindMethods();
     this._addEventListeners();
   }
 
@@ -13,8 +14,12 @@ export class Counter {
     this.quantityDisplay = this.anchorElement.querySelector('.counter__quantity-display');
   }
 
+  _bindMethods() {
+    this._handleCounterClick = this._handleCounterClick.bind(this);
+  }
+
   _addEventListeners() {
-    this.counter.addEventListener('click', this._handleCounterClick.bind(this));
+    this.counter.addEventListener('click', this._handleCounterClick);
   }
 
   _handleCounterClick(event) {
