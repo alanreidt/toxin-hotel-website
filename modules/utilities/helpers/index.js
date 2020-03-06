50c7b05db524c4a6148ac135528f2bb5ef86b3e9
Alan Reid <alanreidt@gmail.com>
2020-03-06 16:38:40 +0300

    Update utilities module

---
+++ modules/utilities/helpers/index.js
@@ -0,0 +1,23 @@
+/* PLOP_INJECT_IMPORT */
+import adjustToStep from './adjustToStep';
+import replaceAt from './replaceAt';
+import restrictByNeighbors from './restrictByNeighbors';
+import crossWith from './crossWith';
+import eitherOr from './eitherOr';
+import placeBetween from './placeBetween';
+import adjustToRange from './adjustToRange';
+import findValueByRatioBetween from './findValueByRatioBetween';
+import findValuePositionBetween from './findValuePositionBetween';
+
+export {
+  /* PLOP_INJECT_EXPORT */
+  adjustToStep,
+  replaceAt,
+  restrictByNeighbors,
+  crossWith,
+  eitherOr,
+  placeBetween,
+  adjustToRange,
+  findValueByRatioBetween,
+  findValuePositionBetween,
+};
