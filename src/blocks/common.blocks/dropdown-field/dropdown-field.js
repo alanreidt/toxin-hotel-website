d35ae7569b0079a41b8a09b7d8093bc6b4f0e1e6
Alan Reid <alanreidt@gmail.com>
2020-03-07 10:06:16 +0300

    Update dropdown-field.js Replace double quotes with single ones

--- src/blocks/common.blocks/dropdown-field/dropdown-field.js
+++ src/blocks/common.blocks/dropdown-field/dropdown-field.js
@@ -6,19 +6,19 @@ export class DropdownField {
   }
 
   _addHostEventListener() {
-    this.anchorElement.addEventListener("focusin", this._handleParentFocusIn.bind(this));
-    document.addEventListener("click", this._handleDocumentClick.bind(this));
+    this.anchorElement.addEventListener('focusin', this._handleParentFocusIn.bind(this));
+    document.addEventListener('click', this._handleDocumentClick.bind(this));
   }
 
   _handleParentFocusIn() {
-    this.anchorElement.classList.add("dropdown_is-expanded");
+    this.anchorElement.classList.add('dropdown_is-expanded');
   }
 
   _handleDocumentClick(event) {
-    const target = event.target.closest(".js-dropdown");
+    const target = event.target.closest('.js-dropdown');
 
     if (target !== this.anchorElement) {
-      this.anchorElement.classList.remove("dropdown_is-expanded");
+      this.anchorElement.classList.remove('dropdown_is-expanded');
     }
   }
 }
