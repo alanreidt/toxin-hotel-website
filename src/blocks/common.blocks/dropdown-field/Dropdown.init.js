5baaf53d633c16a8ea1dd5a885661b1d2c34628d
Alan Reid <alanreidt@gmail.com>
2020-03-08 13:33:18 +0300

    Correct Dropdown.init.js initial options

--- src/blocks/common.blocks/dropdown-field/Dropdown.init.js
+++ src/blocks/common.blocks/dropdown-field/Dropdown.init.js
@@ -6,19 +6,22 @@ function handleWindowLoad() {
   const dropdowns = document.querySelectorAll('.js-dropdown');
 
   dropdowns.forEach((dropdown) => {
-    Dropdown.create(dropdown, {
-      optionsSet: [
-        {
-          name: 'Adult',
-          value: 2,
-        },
-        {
-          name: 'Child',
-        },
-        {
-          name: 'Baby',
-        }
-      ],
-    });
+    Dropdown.create(dropdown, [
+      {
+        name: 'Adult',
+        boundaries: [1, 5],
+        value: 0,
+      },
+      {
+        name: 'Child',
+        boundaries: [0, 5],
+        value: 0,
+      },
+      {
+        name: 'Baby',
+        boundaries: [0, 3],
+        value: 0,
+      }
+    ]);
   });
 };
